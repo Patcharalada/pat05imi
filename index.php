1f413968207d5fdfeceb4f2d7e10e7d6de279716
Patcharalada <88876417+Patcharalada@users.noreply.github.com>
2021-08-18 22:14:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<h2>Patcharalada</h2>
+<h2>Patcharalada Thongkleang</h2>
+
+<iframe src = https://thingspeak.com/channels/1458422/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+  <br></br>
+ <iframe src = https://thingspeak.com/channels/1458422/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+<br></br>
+  <iframe src = https://thingspeak.com/channels/1458422/maps/channel_show  style="height:45%;width:35%;"></iframe>
