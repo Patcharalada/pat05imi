26dbf3e8321e0d2af3381fe67a1accce4062d1ca
Patcharalada <88876417+Patcharalada@users.noreply.github.com>
2021-08-18 23:10:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
-<h2>Patcharalada Thongkleang</h2>
+<h1>Patcharalada Thongkleang</h1>
+<h2>Humidity</h2>
+<iframe src = https://thingspeak.com/channels/1458422/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="border:none"; height="300px" width="500px"></iframe>
+<h2>Temperature</h2>
+<iframe src = https://thingspeak.com/channels/1458422/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="border:none"; height="300px" width="500px"></iframe>
+<h2>Map</h2>
+<iframe src = https://thingspeak.com/channels/1458422/maps/channel_show  style="border:none"; height="300px" width="500px"></iframe>
 
-<iframe src = https://thingspeak.com/channels/1458422/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
-  <br></br>
- <iframe src = https://thingspeak.com/channels/1458422/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
-<br></br>
-  <iframe src = https://thingspeak.com/channels/1458422/maps/channel_show  style="height:45%;width:35%;"></iframe>
+<html>
+<head>
+<script type="text/JavaScript">
+function timedRefresh(timeoutPeriod)
+{
+    setTimeout("location.reload(true);",timeoutPeriod);
+}
+</script>
+</head>
+  
+<body onload="JavaScript:timedRefresh(15000);">
+
+<?php
+  $hum = file_get_contents('https://api.thingspeak.com/channels/1458422/feeds.json?results=1');
+  $temp = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/2/last.txt');
+
+ echo "<br> temperature is = ".$temp."<br>";
+ echo "humidity is = ".$hum;
+?>
+
+</body>
+</html>
