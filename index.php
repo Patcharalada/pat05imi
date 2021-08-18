f979367e13f505b1343a2497f5ba6bf2ab295db0
Patcharalada <88876417+Patcharalada@users.noreply.github.com>
2021-08-19 01:07:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <h1>62106554 Patcharalada Thongkleang</h1>
     
 <h2>Humidity</h2>
-<iframe src = "https://thingspeak.com/channels/1458422/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"  style="border:none"; height="300px" width="500px"></iframe>
+<iframe src = "https://thingspeak.com/channels/1458422/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"  
+        style="border:none"; height="300px" width="500px"></iframe>
 <h2>Temperature</h2>
-<iframe src = "https://thingspeak.com/channels/1458422/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"  style="border:none"; height="300px" width="500px"></iframe>
+<iframe src = "https://thingspeak.com/channels/1458422/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"  
+        style="border:none"; height="300px" width="500px"></iframe>
 <h2>Map</h2>
 <iframe src = "https://thingspeak.com/channels/1458422/maps/channel_show"  style="border:none"; height="300px" width="500px"></iframe>
 
